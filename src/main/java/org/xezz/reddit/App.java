c9fdbc630280f7fdcf6ede0dc69cbb7c456f0aaa
Xezz <koch@xezz.org>
2013-06-18 18:08:01 +0200

    UPDATE: Implemented algorithm to find the longest Chain

--- src/main/java/org/xezz/reddit/App.java
+++ src/main/java/org/xezz/reddit/App.java
@@ -1,5 +1,8 @@
 package org.xezz.reddit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Reddit challenge 127
  * http://www.reddit.com/r/dailyprogrammer/comments/1g09qy/060913_challenge_127_intermediate_call_forwarding/
@@ -11,8 +14,36 @@ public class App {
         final String firstLine = "0000 0001 1 3";
         final String secondLine = "0001 4964 2 1";
         final String thirdLine = "4964 0005 2 3";
+        final String forth = "9555 9556 2 3";
+        final String fifth = "0005 1234 2 3";
+        final String sixth = "1234 9874 2 3";
+        final String seventh = "9874 1999 2 3";
+        final String eighth = "0012 2354 2 3";
+        final String nineth = "2354 9899 2 3";
+        final String tenth = "5555 6666 2 3";
 
         final String dayToLookFor = "2";
+
+        List<PhoneForward> forwards = new ArrayList<PhoneForward>();
+        forwards.add(ForwardingParser.parsePhoneForward(firstLine));
+        forwards.add(ForwardingParser.parsePhoneForward(secondLine));
+        forwards.add(ForwardingParser.parsePhoneForward(thirdLine));
+        forwards.add(ForwardingParser.parsePhoneForward(forth));
+        forwards.add(ForwardingParser.parsePhoneForward(fifth));
+        forwards.add(ForwardingParser.parsePhoneForward(sixth));
+        forwards.add(ForwardingParser.parsePhoneForward(seventh));
+        forwards.add(ForwardingParser.parsePhoneForward(eighth));
+        forwards.add(ForwardingParser.parsePhoneForward(nineth));
+        forwards.add(ForwardingParser.parsePhoneForward(tenth));
+        CallForwarder cf = new CallForwarder(forwards);
+        final int day = Integer.parseInt(dayToLookFor);
+        final int longestChainByDay = cf.getLongestChainByDay(day);
+        final int totalForwardingsByDay = cf.getTotalForwardingsByDay(day);
+        System.out.println("Total Forwardings: " + totalForwardingsByDay);
+        System.out.println("Longest Chain: " + longestChainByDay);
+
+        System.out.println("Forwards for not covered day " + cf.getTotalForwardingsByDay(19));
+        System.out.println("Chains for not covered day " + cf.getLongestChainByDay(19));
 
 
     }
--- src/main/java/org/xezz/reddit/CallForwarder.java
+++ src/main/java/org/xezz/reddit/CallForwarder.java
@@ -1,9 +1,71 @@
 package org.xezz.reddit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: Xezz
  * Date: 18.06.13
  * Time: 16:57
  */
 public class CallForwarder {
+    private List<PhoneForward> phoneForwards;
+
+    public CallForwarder(List<PhoneForward> forwards) {
+        this.phoneForwards = forwards;
+    }
+
+    public int getTotalForwardingsByDay(final int day) {
+        return getAllForwardsByDay(day).size();
+    }
+
+    private List<PhoneForward> getAllForwardsByDay(final int day) {
+        List<PhoneForward> specificForwards = new ArrayList<PhoneForward>();
+        for (PhoneForward pf : phoneForwards) {
+            if (pf.isDayInVacation(day)) {
+                specificForwards.add(pf);
+            }
+        }
+        return specificForwards;
+    }
+
+    public int getLongestChainByDay(final int day) {
+        final List<PhoneForward> allForwardsByDay = getAllForwardsByDay(day);
+        if (allForwardsByDay.size() == 0) {
+            return 0;
+        }
+        int chainLength = 0;
+        while (allForwardsByDay.size() >= chainLength) {
+            PhoneForward p = allForwardsByDay.remove(0);
+            int currentCount = 1;
+            currentCount = getAmountOfPrecursorsAndRemoveThem(p, allForwardsByDay, currentCount);
+            currentCount = getAmountOfSuccessorsAndRemoveThem(p, allForwardsByDay, currentCount);
+            if (currentCount > chainLength) {
+                chainLength = currentCount;
+            }
+        }
+        return chainLength;
+    }
+
+    private int getAmountOfPrecursorsAndRemoveThem(PhoneForward pf, List<PhoneForward> list, int currentCount) {
+        for (PhoneForward value : list) {
+            if (value.getTargetNumber().equals(pf.getOwnNumber())) {
+                list.remove(value);
+                currentCount++;
+                return getAmountOfPrecursorsAndRemoveThem(value, list, currentCount);
+            }
+        }
+        return currentCount;
+    }
+
+    private int getAmountOfSuccessorsAndRemoveThem(PhoneForward pf, List<PhoneForward> list, int currentCount) {
+        for (PhoneForward value : list) {
+            if (value.getOwnNumber().equals(pf.getTargetNumber())) {
+                list.remove(value);
+                currentCount++;
+                return getAmountOfSuccessorsAndRemoveThem(value, list, currentCount);
+            }
+        }
+        return currentCount;
+    }
 }
